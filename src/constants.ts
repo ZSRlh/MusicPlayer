19aad5402a780c8dd5d8d8b980d767c5d04d467e
ZSRlh <952177238@qq.com>
2021-11-26 22:22:42 +0800

    feat: add browser tab title

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const TAB_TITLE: Record<string, string> = {
+  APP_NAME: "ZSR Music",
+  DISCOVER: "发现音乐",
+  MY: "我的音乐",
+  FRIEND: "朋友",
+};
--- src/router/index.ts
+++ src/router/index.ts
@@ -12,6 +12,10 @@ import MyMV from "@/pages/MyMV/index.vue";
 import MyPlaylist from "@/pages/MyPlaylist/index.vue";
 import Friend from "@/pages/Friend/index.vue";
 
+import { TAB_TITLE } from "../constants";
+
+const { APP_NAME, DISCOVER, MY, FRIEND } = TAB_TITLE;
+
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
@@ -27,26 +31,41 @@ const routes: Array<RouteConfig> = [
         path: "",
         name: "Recommend",
         component: Recommend,
+        meta: {
+          title: `${APP_NAME} | ${DISCOVER} | 推荐`,
+        },
       },
       {
         path: "toplist",
         name: "Toplist",
         component: Toplist,
+        meta: {
+          title: `${APP_NAME} | ${DISCOVER} | 排行榜`,
+        },
       },
       {
         path: "playlist",
         name: "Playlist",
         component: Playlist,
+        meta: {
+          title: `${APP_NAME} | ${DISCOVER} | 歌单`,
+        },
       },
       {
         path: "radio",
         name: "Radio",
         component: Radio,
+        meta: {
+          title: `${APP_NAME} | ${DISCOVER} | 电台`,
+        },
       },
       {
         path: "artist",
         name: "Artist",
         component: Artist,
+        meta: {
+          title: `${APP_NAME} | ${DISCOVER} | 歌手`,
+        },
       },
     ],
   },
@@ -59,21 +78,33 @@ const routes: Array<RouteConfig> = [
         path: "artist",
         name: "MyArtist",
         component: MyArtist,
+        meta: {
+          title: `${APP_NAME} | ${MY} | 我的歌手`,
+        },
       },
       {
         path: "radio",
         name: "MyRadio",
         component: MyRadio,
+        meta: {
+          title: `${APP_NAME} | ${MY} | 我的电台`,
+        },
       },
       {
         path: "mv",
         name: "MyMV",
         component: MyMV,
+        meta: {
+          title: `${APP_NAME} | ${MY} | 我的视频`,
+        },
       },
       {
         path: "playlist",
         name: "MyPlaylist",
         component: MyPlaylist,
+        meta: {
+          title: `${APP_NAME} | ${MY} | 我的歌单`,
+        },
       },
     ],
   },
@@ -85,6 +116,9 @@ const routes: Array<RouteConfig> = [
         path: "",
         name: "Friend",
         component: Friend,
+        meta: {
+          title: `${APP_NAME} | ${FRIEND}`,
+        },
       },
     ],
   },
@@ -94,6 +128,10 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.afterEach((to) => {
+  document.title = to?.meta?.title || APP_NAME;
 });
 
 export default router;
